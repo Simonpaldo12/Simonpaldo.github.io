736f508dd7b7532c3d0d1ba197d4041923e1abc1
Simonpaldo12 <johnsimoncabrera30@gmail.com>
2024-06-08 14:35:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,14 +13,11 @@ function clickLike2(){
 }
 btnLike1.addEventListener("click",clickLike1)
 btnLike2.addEventListener("click",clickLike2)
-const submit document.getElementById("submit")
 
-const comment document.getElementById("comment")
-
-const commentbox document.getElementById("commentbox")
-
+const submit = document.getElementById("submit")
+const comment = document.getElementById("comment")
+const commentbox = document.getElementById("commentbox")
 function submitComment(){
-
-commentbox.textContent comment.value.toString() + "\n" }
-
+  commentbox.textContent += comment.value.toString() + "\n"
+}
 submit.addEventListener("click", submitComment)
