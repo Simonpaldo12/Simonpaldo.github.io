9aee195f36f1da812e017d3a81c5121e1164bae9
Simonpaldo12 <johnsimoncabrera30@gmail.com>
2024-06-08 14:48:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const countLikes2 = document.getElementById("countLikes2")
 const submit = document.getElementById("submit")
 const comment = document.getElementById("comment")
 const commentbox = document.getElementById("commentbox")
-
+function submitComment(){
+  commentbox.textContent += comment.value.toString() + "\n"
+}
 function clickLike1(){
     let totalLikes1 = parseInt(countLikes1.value) + 1
     countLikes1.textContent = totalLikes1.toString()
